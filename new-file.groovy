d33c2c785d78d389ef40899226a65935dbe3e969
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-08-29 12:21:09 +0530

    Add files via upload

---
+++ new-file.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+
+    environment {
+        PYTHON_VERSION = '3.8'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Vaibhav190993/test-jenkins.git'
+            }
+        }
+        stage('Setup') {
+            steps {
+                script {
+                    sh '''
+                    python${PYTHON_VERSION} -m venv venv
+                    . venv/bin/activate
+                    pip install -r requirements.txt
+                    '''
+                }
+            }
+        }
+        stage('Run Python Script') {
+            steps {
+                script {
+                    sh '''
+                    . venv/bin/activate
+                    python main.py
+                    '''
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            // Clean up or perform any actions after the pipeline run
+            echo 'Pipeline finished.'
+        }
+    }
+}
