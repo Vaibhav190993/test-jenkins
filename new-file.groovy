0ce36edc9c62aa9c89c82f74ff7ae7fd973da459
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-08-29 13:13:59 +0530

    Update new-file.groovy

--- new-file.groovy
+++ new-file.groovy
@@ -11,11 +11,21 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/Vaibhav190993/test-jenkins.git'
             }
         }
+        stage('Verify Python Installation') {
+            steps {
+                script {
+                    sh '''
+                    which python3
+                    python3 --version
+                    '''
+                }
+            }
+        }
         stage('Setup') {
             steps {
                 script {
                     sh '''
-                    python$/usr/bin/python3 -m venv venv
+                    ${PYTHON_VERSION} -m venv venv
                     . venv/bin/activate
                     pip install -r requirements.txt
                     '''
