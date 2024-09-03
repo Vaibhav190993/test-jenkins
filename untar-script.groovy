afdfed59c0391d10ae2788400f1921bf246fa2e7
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-03 14:49:06 +0530

    Update untar-script.groovy

--- untar-script.groovy
+++ untar-script.groovy
@@ -1,30 +1,19 @@
 pipeline {
     agent any
 
-    environment {
-        TAR_FILE = "fo_installer-RHEL8.6_23.0.0_FP_03_635338.tar"   // Path to your tar.gz file within the workspace
-        DEST_DIR = "/home/ec2-user"     // Directory to extract the contents
-    }
-
     stages {
         stage('Checkout') {
             steps {
-                // Checkout your repository
-                git 'https://github.com/Vaibhav190993/test-jenkins.git'
+
+            git branch: 'main', url: 'https://github.com/Vaibhav190993/test-jenkins.git'
+            
             }
         }
 
-        stage('Run as Root') {
+        stage('Untar File using Python') {
             steps {
-                script {
-                    // Run commands as root using sudo -i
-                    sh '''
-                        sudo -i <<EOF
-                        mkdir -p ${DEST_DIR}
-                        tar -xvf ${TAR_FILE} -C ${DEST_DIR}
-                        EOF
-                    '''
-                }
+                // Run the Python script
+                sh 'python3 /path/to/untar_script.py'
             }
         }
     }
