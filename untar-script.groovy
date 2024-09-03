55dfdd6603387d53dc5e6ecc387603e9c71dd11a
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-03 13:47:38 +0530

    Update untar-script.groovy

--- untar-script.groovy
+++ untar-script.groovy
@@ -1,52 +1,20 @@
 pipeline {
     agent any
 
-    environment {
-        TAR_FILE = "fo_installer-RHEL8.6_23.0.0_FP_03_635338.tar"   // Path to your tar.gz file within the workspace
-        DEST_DIR = "/home/ec2-user"     // Directory to extract the contents
-    }
-
     stages {
-        stage('Checkout') {
+        stage('Create a File') {
             steps {
-                // Checkout your repository
-                git branch: 'main', url: 'https://github.com/Vaibhav190993/test-jenkins.git'
+                sh 'echo "Hello, World!" > myfile.txt'
             }
         }
 
-        stage('Untar File') {
+        stage('Read a File') {
             steps {
                 script {
-                    untarFile(env.TAR_FILE, env.DEST_DIR)
+                    def content = readFile 'myfile.txt'
+                    echo "File content: ${content}"
                 }
             }
         }
-    }
-
-    post {
-        always {
-            // Placeholder for cleanup or notification steps
-            echo 'Pipeline execution completed.'
-        }
-    }
-}
-
-def untarFile(String tarFilePath, String destDirPath) {
-    File tarFile = new File(tarFilePath)
-    File destDir = new File(destDirPath)
-    
-    if (!destDir.exists()) {
-        destDir.mkdirs()
-    }
-
-    def command = "tar -xvf ${tarFile.absolutePath} -C ${destDir.absolutePath}"
-    def process = command.execute()
-
-    process.waitForProcessOutput(System.out, System.err)
-    
-    if (process.exitValue() == 0) {
-        println "File successfully extracted to ${destDirPath}"
-    } else {
-        println "Error extracting file. Exit code: ${process.exitValue()}"
     }
 }
