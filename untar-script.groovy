e18193eca5028f4730d351ad8c7c80a1e6314ecb
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-03 14:54:13 +0530

    Update untar-script.groovy

--- untar-script.groovy
+++ untar-script.groovy
@@ -1,26 +1,53 @@
 pipeline {
     agent any
 
+    environment {
+        PYTHON_VERSION = '/usr/bin/python3'
+    }
+
     stages {
         stage('Checkout') {
             steps {
-
-            git branch: 'main', url: 'https://github.com/Vaibhav190993/test-jenkins.git'
-            
+                git branch: 'main', url: 'https://github.com/Vaibhav190993/test-jenkins.git'
             }
         }
-
+        stage('Verify Python Installation') {
+            steps {
+                script {
+                    sh '''
+                    which python3
+                    python3 --version
+                    '''
+                }
+            }
+        }
+        stage('Setup') {
+            steps {
+                script {
+                    sh '''
+                    ${PYTHON_VERSION} -m venv venv
+                    . venv/bin/activate
+                    pip install -r requirements.txt
+                    '''
+                }
+            }
+        }
         stage('Untar File using Python') {
             steps {
-                // Run the Python script
-                sh 'python3 /path/to/untar_script.py'
+                script {
+                    sh '''
+                    . venv/bin/activate
+                    untar_script.py
+                    '''
+                }
             }
         }
     }
 
     post {
         always {
-            echo 'Pipeline execution completed.'
+            // Clean up or perform any actions after the pipeline run
+            echo 'Pipeline finished.'
         }
     }
 }
