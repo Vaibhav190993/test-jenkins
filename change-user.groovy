383b2acaececeaae4bd8a7fbd59ba8b16c592caf
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-05 17:42:34 +0530

    Update change-user.groovy

--- change-user.groovy
+++ change-user.groovy
@@ -4,6 +4,7 @@ pipeline {
     environment {
         SOURCE_FILE = ""  // Path to the source file
         TARGET_FILE = "" // Target path for the copied file
+        DEPLOYMENT_HOST = '10.92.131.112'  // Remote server IP
     }
 
     stages {
@@ -11,7 +12,7 @@ pipeline {
             steps {
                 // Copy the file to the target directory
                 sh """
-                    cp /data/all-envs-and-hosts.yml /data/fo_installer/inventory/yaml/all-envs-and-hosts.yml
+                    ssh cloud-user@${DEPLOYMENT_HOST} cp /data/all-envs-and-hosts.yml /data/fo_installer/inventory/yaml/all-envs-and-hosts.yml
                 """
             }
         }
