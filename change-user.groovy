16c5746e5c8f8026e076347f69981daa6d291d26
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-05 17:30:49 +0530

    Update change-user.groovy

--- change-user.groovy
+++ change-user.groovy
@@ -2,7 +2,6 @@ pipeline {
     agent any
 
     environment {
-        DEPLOYMENT_HOST = '10.92.131.112'  // Remote server IP
         CONFIG_FILE = "/data/configurations.json"  // Path to the configuration file
         TARGET_DIR = "/data/fo_installer"  // Target directory for moving the file
     }
@@ -10,9 +9,9 @@ pipeline {
     stages {
         stage('Copy Configuration File') {
             steps {
-                // Copy the configuration file to the target directory
+                // Copy the configuration file to the target directory locally
                 sh """
-                    cp cloud-user@${DEPLOYMENT_HOST}:${CONFIG_FILE} cloud-user@${DEPLOYMENT_HOST}:${TARGET_DIR}
+                    cp ${CONFIG_FILE} ${TARGET_DIR}
                 """
             }
         }
