b36708c9b1c5990dca9d915a3e75baa2179d0b10
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-05 13:37:06 +0530

    Update final-script.groovy

--- final-script.groovy
+++ final-script.groovy
@@ -42,7 +42,7 @@ pipeline {
         stage('Untar File') {
             steps {
                 // Run the tar command directly
-                sh "tar -xvf ${TAR_FILE}"
+                sh "tar -xvf ${TAR_FILE} -C /data/"
             }
         }
         stage('After Untar File check') {
