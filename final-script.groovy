9cefdc44f04f3314b9b8f820d74311dec7f4cd54
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-05 15:57:23 +0530

    Update final-script.groovy

--- final-script.groovy
+++ final-script.groovy
@@ -5,6 +5,7 @@ pipeline {
         DEPLOYMENT_HOST = '10.92.131.112'  // Remote server IP
         TAR_FILE = "/data/fo_installer-20.11.0_453110.tar"  // Path to the tar file on the remote server
         UNTAR_DIR = "/data"  // Directory where the tar file will be extracted
+        TARGET_DIR = "${UNTAR_DIR}/fo_installer"  // Target directory for moving the tar file
     }
 
     stages {
@@ -17,23 +18,32 @@ pipeline {
             }
         }
 
-        stage('Move and Change Directory') {
+        stage('Create Target Directory') {
             steps {
-                // Move the tar file and change to the 'fo_installer' directory
+                // Create the target directory if it doesn't exist
                 sh """
-                    ssh cloud-user@${DEPLOYMENT_HOST} '
-                    mv /data/*.tar fo_installer &&
-                    cd ${UNTAR_DIR}/fo_installer
-                    '
+                    ssh cloud-user@${DEPLOYMENT_HOST} 'mkdir -p ${TARGET_DIR}'
                 """
             }
         }
 
-        stage('After Untar File Check on Remote') {
+        stage('Move Tar Files') {
             steps {
-                // Check the result of the untar operation on the remote server
+                // Move the tar file to the target directory
                 sh """
-                    ssh cloud-user@${DEPLOYMENT_HOST} 'ls -ld ${UNTAR_DIR}/fo_installer'
+                    ssh cloud-user@${DEPLOYMENT_HOST} 'mv /data/*.tar ${TARGET_DIR}'
+                """
+            }
+        }
+
+        stage('Change Directory and Check') {
+            steps {
+                // Change to the target directory and list its contents
+                sh """
+                    ssh cloud-user@${DEPLOYMENT_HOST} '
+                    cd ${TARGET_DIR} &&
+                    ls -l
+                    '
                 """
             }
         }
