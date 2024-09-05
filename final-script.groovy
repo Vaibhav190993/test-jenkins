a412302facbed1fef2dee0f0982b40935d8d8e04
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-05 12:35:12 +0530

    Add files via upload

---
+++ final-script.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    environment {
+        // Define the SSH credentials ID that Jenkins should use
+        DEPLOYMENT_HOST = '10.92.131.112'
+    }
+
+    stages {
+        stage('SSH and Navigate') {
+            steps {
+                script {
+                    // Use the 'ssh' command to connect and execute commands remotely
+                    sh """
+                        ssh -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no cloud-user@${DEPLOYMENT_HOST} 'cd /data && ls -l'
+                    """
+                }
+            }
+        }
+    }
+}
