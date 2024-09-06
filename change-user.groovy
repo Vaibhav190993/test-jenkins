2aee5ec2c641770d9a2c0a03adba0bc7ed6d9129
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-06 19:04:45 +0530

    Update change-user.groovy

--- change-user.groovy
+++ change-user.groovy
@@ -10,7 +10,7 @@ pipeline {
             steps {
                 // SSH into the remote server
                 sh """
-                ssh cloud-user@${DEPLOYMENT_HOST}
+                su - cloud-user
                 """
             }
         }
