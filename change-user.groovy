eab360a0273b1ab828350e03c628d8c1f3c6b947
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-06 21:58:55 +0530

    Update change-user.groovy

--- change-user.groovy
+++ change-user.groovy
@@ -3,14 +3,82 @@ pipeline {
 
     environment {
         DEPLOYMENT_HOST = '10.91.45.198'  // Remote server IP
+        TAR_FILE = "/data/fo_installer-ROCKY8.6_23.0.0_FP_03_638238.tar"  // Path to the tar file on the remote server
+        UNTAR_DIR = "/data"  // Directory where the tar file will be extracted
+        TARGET_DIR = "${UNTAR_DIR}/fo_installer"  // Target directory for moving the tar file
     }
 
     stages {
-        stage('Verify User on Remote Server') {
+        stage('Untar File on Remote') {
             steps {
-                // Verify the user running commands on the remote server
+                // Use SSH to untar the file on the remote server
                 sh """
-                whoami
+                    tar -xvf ${TAR_FILE} -C ${UNTAR_DIR}
+                """
+            }
+        }
+
+        stage('Create Target Directory') {
+            steps {
+                // Create the target directory if it doesn't exist
+                sh """
+                    mkdir -p ${TARGET_DIR}
+                """
+            }
+        }
+
+        stage('Move Tar Files') {
+            steps {
+                // Move the tar file to the target directory
+                sh """
+                    mv /data/*.tar ${TARGET_DIR}
+                """
+            }
+        }
+
+        stage('Copy Configuration File') {
+            steps {
+                // Copy the configuration file to the target directory locally
+                sh """
+                    cp /data/configurations.json /data/fo_installer
+                """
+            }
+        }
+
+        stage('Copy Yaml File') {
+            steps {
+                // Copy the file to the target directory
+                sh """
+                    cp /data/all-envs-and-hosts.yml /data/fo_installer/inventory/yaml/all-envs-and-hosts.yml
+                """
+            }
+        }
+
+        stage('Initialize FlowOne Fulfillment deployment') {
+            steps {
+                // Initialize the deployment
+                sh """
+                    /data/fo_installer/init_deployment.sh
+                """
+            }
+        }
+
+        stage('Change Directory and Check') {
+            steps {
+                // Change to the target directory and list its contents
+                sh """
+                    cd ${TARGET_DIR} &&
+                    ls -l
+                """
+            }
+        }
+
+        stage('Initialize FlowOne Fulfillment deployment') {
+            steps {
+                // Run the init_deployment.sh script
+                sh """
+                    cd /data/fo_installer/ &&
+                    ./init_deployment.sh
                 """
             }
         }
