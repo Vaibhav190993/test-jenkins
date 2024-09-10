503b7565810849b36ca34def8a47b284b6e44275
Vaibhav190993 <85166021+Vaibhav190993@users.noreply.github.com>
2024-09-10 15:41:08 +0530

    Update final-script.groovy

--- final-script.groovy
+++ final-script.groovy
@@ -2,8 +2,8 @@ pipeline {
     agent any
 
     environment {
-        DEPLOYMENT_HOST = '10.92.131.112'  // Remote server IP
-        TAR_FILE = "/data/fo_installer-20.11.0_453110.tar"  // Path to the tar file on the remote server
+        DEPLOYMENT_HOST = '10.91.45.198'  // Remote server IP
+        TAR_FILE = "/data/fo_installer-ROCKY8.6_23.0.0_FP_03_638238.tar"  // Path to the tar file on the remote server
         UNTAR_DIR = "/data"  // Directory where the tar file will be extracted
         TARGET_DIR = "${UNTAR_DIR}/fo_installer"  // Target directory for moving the tar file
     }
@@ -13,7 +13,7 @@ pipeline {
             steps {
                 // Use SSH to untar the file on the remote server
                 sh """
-                    ssh cloud-user@${DEPLOYMENT_HOST} 'tar -xvf ${TAR_FILE} -C ${UNTAR_DIR}'
+                    'tar -xvf ${TAR_FILE} -C ${UNTAR_DIR}'
                 """
             }
         }
@@ -22,7 +22,7 @@ pipeline {
             steps {
                 // Create the target directory if it doesn't exist
                 sh """
-                    ssh cloud-user@${DEPLOYMENT_HOST} 'mkdir -p ${TARGET_DIR}'
+                    'mkdir -p ${TARGET_DIR}'
                 """
             }
         }
@@ -31,7 +31,32 @@ pipeline {
             steps {
                 // Move the tar file to the target directory
                 sh """
-                    ssh cloud-user@${DEPLOYMENT_HOST} 'mv /data/*.tar ${TARGET_DIR}'
+                    'mv /data/*.tar ${TARGET_DIR}'
+                """
+            }
+        }
+
+        stage('Copy Configuration File') {
+            steps {
+                // Copy the configuration file to the target directory locally
+                sh """
+                    cp /data/configurations.json /data/fo_installer
+                """
+            }
+        }
+        stage('Copy Yaml File') {
+            steps {
+                // Copy the file to the target directory
+                sh """
+                    cp /data/all-envs-and-hosts.yml /data/fo_installer/inventory/yaml/all-envs-and-hosts.yml
+                """
+            }
+        }
+        stage('Initialize FlowOne Fulfillment deployment') {
+            steps {
+                // Copy the file to the target directory
+                sh """
+                    '/data/fo_installer/init_deployment.sh'
                 """
             }
         }
@@ -40,10 +65,69 @@ pipeline {
             steps {
                 // Change to the target directory and list its contents
                 sh """
-                    ssh cloud-user@${DEPLOYMENT_HOST} '
+                    '
                     cd ${TARGET_DIR} &&
                     ls -l
                     '
+                """
+            }
+        }
+        stage('Initialize FlowOne Fulfillment deployment') {
+            steps {
+                // Copy the file to the target directory
+                sh """
+                cd /data/fo_installer/ &&
+                    ./init_deployment.sh
+                """
+            }
+        }
+        stage('Run infrastructure Script') {
+            steps {
+                // Run the fo-infrastructure.sh script
+                sh """
+                    cd /data/fo_installer/ &&
+                    chmod +x install-fo-infrastructure.sh &&
+                    ./install-fo-infrastructure.sh
+                """
+            }
+        }
+        stage('Run Fo Components Script') {
+            steps {
+                // Run the fo-components-om script
+                sh """
+                    cd /data/fo_installer/ &&
+                    chmod +x install-fo-components-om.sh &&
+                    ./install-fo-components-om.sh
+                """
+            }
+        }
+        stage('Run Fo Components Catalog Script') { 
+            steps {
+                // Run the fo-components-catalog script
+                sh """
+                    cd /data/fo_installer/ &&
+                    chmod +x install-fo-components-catalog.sh &&
+                    ./install-fo-components-catalog.sh
+                """
+            }
+        }
+        stage('Run Fo Components flowoneapi Script') {
+            steps {
+                // Run the fo-components-flowoneapi script
+                sh """
+                    cd /data/fo_installer/ &&
+                    chmod +x install-fo-components-flowoneapi.sh &&
+                    ./install-fo-components-flowoneapi.sh
+                """
+            }
+        }
+        stage('Run Fo Components wso2 Script') {
+            steps {
+                // Run the fo-components-wso2 script
+                sh """
+                    cd /data/fo_installer/ &&
+                    chmod +x install-fo-components-wso2.sh &&
+                    ./install-fo-components-wso2.sh
                 """
             }
         }
